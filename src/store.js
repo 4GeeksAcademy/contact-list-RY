23e37b71c679616866f2458488a0c5dbb3d3932b
Rubeny94 <44436716+rubenyanes94@users.noreply.github.com>
2025-11-24 23:41:58 +0000

    [create a logic for store. Use this data for my agenda: ruben]

--- src/store.js
+++ src/store.js
@@ -1,32 +1,39 @@
-export const initialStore=()=>{
-  return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
-  }
-}
+// store.js
+export const initialStore = () => {
+	return {
+		message: null,
+		todos: [
+			{ id: 1, title: "Make the bed", background: null },
+			{ id: 2, title: "Do my homework", background: null }
+		],
+		contacts: [] 
+	};
+};
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'add_task':
+	switch (action.type) {
+		case "add_task":
+			const { id, color } = action.payload;
+			return {
+				...store,
+				todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+			};
+		
+		case "load_contacts":
+			return {
+				...store,
+				contacts: action.payload
+			};
 
-      const { id,  color } = action.payload
+		case "delete_contact":
+			// Eliminar visualmente el contacto del store
+			return {
+				...store,
+				contacts: store.contacts.filter((c) => c.id !== action.payload)
+			};
+			
 
-      return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
-    default:
-      throw Error('Unknown action.');
-  }    
+		default:
+			throw Error("Unknown action.");
+	}
 }
